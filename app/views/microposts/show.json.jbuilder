88fad6d0690c34b454294b09b6a17c652d6b29cf
Alberto Blanco <albertoblanco.s@gmail.com>
2014-09-06 17:01:08 -0700

    Initial commit

---
+++ app/views/microposts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @micropost, :id, :content, :User_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testing::Application.config.secret_key_base = 'cdc6d0b48ad7be61f5d04fb280a446591334bea0b6535b0270b324955e624b366792a818669cdc468cd1928b38d632de18f96a38cacfe5f7d3b9918af439d590'
